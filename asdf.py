029d12fd2254892b9eb741ab11114fe1301f9072
ldj9876 <31953980+ldj9876@users.noreply.github.com>
2021-01-21 12:52:43 +0900

    Add files via upload

--- asdf.py
+++ asdf.py
@@ -11,6 +11,11 @@ class args:
     n_craftstep = 60
     craft_rate = 200
     craft_rate_drop_period = 20
+
+    model_replay = 8
+    model_name = 'resnet50'
+    learning_rate = 0.1
+
 def get_resnet(model_name,input_shape, num_class=10):
     if model_name == 'resnet50':
         model = keras.applications.ResNet50(include_top=True, weights=None,classes=num_class,input_shape=input_shape)
@@ -24,14 +29,33 @@ def get_resnet(model_name,input_shape, num_class=10):
 
     return model
 
-loss = tf.reduce_mean
+def model_initalize(model):
+    pass
 
 
 n_craftstep = args.n_craftstep
 craft_rate = args.craft_rate
 craft_rate_drop_period = args.craft_rate_drop_period
+model_replay = args.model_replay
+model_name = args.model_name
+learning_rate = args.learning_rate
 
-for craftstep in range(n_craftstep):
+
+train_loss = keras.losses.categorical_crossentropy
+model = get_resnet(model_name,(32,32,3))
+
+for craftstep in range(1,n_craftstep+1):
     if craftstep % craft_rate_drop_period == 0:
         craft_rate = craft_rate / 10
+
+    model_initalize(model)
+    for replay in range(model_replay):
+        print(craftstep,replay)
+
+
+
+
+
+
+
 
