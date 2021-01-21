1aa88f0b05a6bd95fd2ddc64464067600bd1780c
ldj9876 <31953980+ldj9876@users.noreply.github.com>
2021-01-21 16:25:16 +0900

    Update asdf.py

--- asdf.py
+++ asdf.py
@@ -15,6 +15,7 @@ class args:
     model_replay = 8
     model_name = 'resnet50'
     learning_rate = 0.1
+    n_adapt = 2 # in algorithm unrolled step K
 
 def get_resnet(model_name,input_shape, num_class=10):
     if model_name == 'resnet50':
@@ -31,7 +32,10 @@ def get_resnet(model_name,input_shape, num_class=10):
 
 def model_initalize(model):
     pass
-
+def get_batch():
+    pass
+def adv_loss():
+    pass
 
 n_craftstep = args.n_craftstep
 craft_rate = args.craft_rate
@@ -39,19 +43,41 @@ craft_rate_drop_period = args.craft_rate_drop_period
 model_replay = args.model_replay
 model_name = args.model_name
 learning_rate = args.learning_rate
-
+batch_size = args.batch_size
 
 train_loss = keras.losses.categorical_crossentropy
 model = get_resnet(model_name,(32,32,3))
+mod = model.get_weights()
+
+lr_schedule = keras.optimizers.schedules.ExponentialDecay(learning_rate,
+                                                          decay_steps=50,
+                                                          decay_rate=10)
+train_optimizer = keras.optimizers.SGD(learning_rate = lr_schedule)
+
 
 for craftstep in range(1,n_craftstep+1):
     if craftstep % craft_rate_drop_period == 0:
         craft_rate = craft_rate / 10
 
-    model_initalize(model)
     for replay in range(model_replay):
-        print(craftstep,replay)
+        model_initalize(model)
+        adv_loss_hist = []
 
+        prev_weight = model.get_weights()
+        for _ in range(args.n_adapt):
+            x_batch, y_batch = get_batch()
+            with tf.GradientTape as tape:
+                logits = model(x_batch, training = True)
+                loss_value = train_loss(y_batch,logits)
+            grads = tape.gradient(loss_value,model.trainable_weights)
+
+            train_optimizer.apply_gradients(zip(grads,model.trainable_weights))
+
+        adv_logit = model(targets, training = False)
+        loss = adv_loss(adv_label, adv_logit)
+        adv_loss_hist.append(loss)
+
+        model.set_weights(prev_weight)
 
 
 
